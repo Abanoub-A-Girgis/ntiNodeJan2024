5302c263b03fb3e1ae8e733744c34bb31007e81d
Abanoub Girgis Aziz Girgis <bebo.hema.95@gmail.com>
2024-01-01 22:20:25 +0200

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+// console.log("Hello !!!", "first Session....");
+
+x = 10.357
+y = "hi eveeryone";
+z = true;
+
+var a = 11;
+let b = "good night";
+
+a= 33;
+a = "other data";
+
+// constants
+const w = "get the value";
+//w = other; error
+
